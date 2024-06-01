db9eb711442dfe496c664896f0cad0e0eafef678
Omkar <66915267+omgol411@users.noreply.github.com>
2024-06-01 11:35:31 +0530

    Add files via upload

---
+++ colabfold_run.py
@@ -0,0 +1,42 @@
+## This script can be used to run local colabfold ##
+## Please go through the brief information about local colabfold given - https://wakeful-talk-e86.notion.site/local-ColabFold-72425fb5c373471db76a2a90b3158bde?pvs=4 ##
+## by OMG-10/04/2024 ##
+
+import os
+
+input_fasta = "./fasta_files" # specify path to the fasta/a3m file or provide a path to the directory containing fasta files 
+ouput_dir = "./output_models" # specify path to the output directory
+pair_mode = "unpaired_paired" # paired , unpaired , unpaired_paired
+msa_mode = "mmseqs2_uniref_env" # mmseqs2_uniref_env , mmseqs2_uniref , single_sequence
+custom_template_path = "None" # specify path to the directory containg the PDB files
+num_recycle = "3" # can be set till 20 (will increase prediction time, ColabFold default value is 3)
+num_seeds = "1" # to generate more than one model from each NN
+random_seed = "9" 
+num_models = "5" # how many NN to use (upto 5; ColabFold default is 5)
+model_type = "auto" # auto , alphafold2 , alphafold2_ptm , alphafold2_multimer_v1 , alphafold2_multimer_v2 , alphafold2_multimer_v3, deepfold_v1
+max_msa = "8:16" # set as 'max-seq:max-extra-seq' (lower values will sample alternate conformations, read https://elifesciences.org/articles/75751)
+num_relax = "1"
+
+
+
+#os.system("colabfold_batch -h")
+os.system(
+'colabfold_batch '
+f'{input_fasta} '
+f'{ouput_dir} '
+f'--pair-mode {pair_mode} '
+f'--msa-mode {msa_mode} '
+f'--num-recycle {num_recycle} '
+f'--num-seeds {num_seeds} '
+#f'--templates '
+#f'--custom-template-path {custom_template_path} '
+f'--random-seed {random_seed} '
+f'--num-models {num_models} '
+f'--model-type {model_type} '
+#f'--max-msa {max_msa} '
+#f'--amber '
+#f'--num-relax {num_relax} '
+#f'--use-gpu-relax '
+f'--overwrite-existing-results '
+#f'--save-recycles '
+)
---
+++ generate_pairwise_fasta.py
@@ -0,0 +1,50 @@
+import os
+from pprint import pprint 
+
+fragments = './MreB_fragments.txt'
+savepath = './fasta_files'
+
+all_lines = []
+with open(fragments) as f:
+    while line := f.readline():
+        a = line.rstrip()
+        if a != '':
+            all_lines.append(a)
+           
+#pprint(all_lines)
+
+prot_dict = {}
+head_count = []
+
+all_text = '__'.join(all_lines)
+
+#print(all_text)
+
+separate_proteins = all_text.split('>')
+separate_proteins = list(filter(None, separate_proteins))
+for p in separate_proteins:
+    p_list = p.split('__')
+    p_list = list(filter(None, p_list))
+#    print(p_list)
+    prot_dict[p_list[0].partition('_')[0]] = {}
+    for i in range(0, len(p_list)):
+        if ':' in p_list[i]:
+            prot_dict[p_list[0].partition('_')[0]][p_list[i][:-1]] = p_list[i+1]
+
+for k1, v1 in prot_dict.items():
+    for k2, v2 in prot_dict.items():
+        #if k1 != k2:
+            for res_range1, res1 in v1.items():
+                for res_range2, res2 in v2.items():
+                    
+                    fasta_head = k1 + '_' + res_range1.replace("-", "_") + "_" + k2 + '_' + res_range2.replace("-", "_")
+                    fasta_alternate_head = k2 + '_' + res_range2.replace("-", "_") + "_" + k1 + '_' + res_range1.replace("-", "_")
+                    fasta_content_1 =  res1 + ':' + '\n'
+                    fasta_content_2 = res2
+                    fastafiles_lst = []
+                    for fastafiles in os.listdir(savepath):
+                        fastafiles_lst.append(fastafiles.partition('.')[0])
+                    if fasta_alternate_head not in fastafiles_lst:
+                        with open(os.path.join(savepath,'{fname}.fasta').format(fname = fasta_head), 'w') as g:
+                            g.writelines(['>'+fasta_head + '\n', fasta_content_1, fasta_content_2])
+#print(fasta_head, fasta_content_1)
